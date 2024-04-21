cdcb100865ae9adee5a7c3b50fadf42fac062af9
Elena Morozova <lenok-tver@yandex.ru>
2024-04-21 20:57:20 +0300

    Updated

--- src/test/java/NewUserTest.java
+++ src/test/java/NewUserTest.java
@@ -5,6 +5,7 @@ import org.openqa.selenium.support.ui.Select;
 
 import java.text.SimpleDateFormat;
 
+import static org.openqa.selenium.support.ui.ExpectedConditions.presenceOfElementLocated;
 import static org.openqa.selenium.support.ui.ExpectedConditions.titleIs;
 
 public class NewUserTest extends TestBase{
@@ -15,24 +16,26 @@ public class NewUserTest extends TestBase{
         System.out.println(email);
         driver.get("http://localhost/litecart/en/");
         driver.findElement(By.linkText("New customers click here")).click();
+        wait.until(presenceOfElementLocated(By.name("firstname")));
         driver.findElement(By.name("firstname")).sendKeys("E");
         driver.findElement(By.name("lastname")).sendKeys("M");
         driver.findElement(By.name("address1")).sendKeys("A1");
         driver.findElement(By.name("postcode")).sendKeys("12345");
         driver.findElement(By.name("city")).sendKeys("S");
-        //Select select = new Select(driver.findElement(By.cssSelector("select[name=country_code]")));
-        //select.selectByValue("US");
-        driver.findElement(By.cssSelector("#create-account span.selection > span")).click();
-        driver.findElement(By.cssSelector("#select2-country_code-mh-result-9xxc-US")).click();
+        driver.findElement(By.cssSelector("#create-account  span.select2-selection__arrow")).click();
+        driver.findElement(By.cssSelector(".select2-results__option[id $=US]")).click();
         driver.findElement(By.name("email")).sendKeys(email);
         driver.findElement(By.name("phone")).sendKeys("777");
         driver.findElement(By.name("password")).sendKeys("123456");
         driver.findElement(By.name("confirmed_password")).sendKeys("123456");
         driver.findElement(By.cssSelector("button[name=create_account]")).click();
+        wait.until(presenceOfElementLocated(By.cssSelector("#box-account a[href*=logout]")));
         driver.findElement(By.cssSelector("#box-account a[href*=logout]")).click();
+        wait.until(presenceOfElementLocated(By.cssSelector("#box-account-login  input[name=email]")));
         driver.findElement(By.cssSelector("#box-account-login  input[name=email]")).sendKeys(email);
         driver.findElement(By.cssSelector("#box-account-login  input[name=password]")).sendKeys("123456");
         driver.findElement(By.cssSelector("#box-account-login  button[name=login]")).click();
+        wait.until(presenceOfElementLocated(By.cssSelector("#box-account a[href*=logout]")));
         driver.findElement(By.cssSelector("#box-account a[href*=logout]")).click();
     }
 }
